fc0fdc8ee1daea9ae0eb9b98b24294141243f42a
kasturisandeep <54030738+kasturisandeep@users.noreply.github.com>
2021-11-22 15:59:45 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,5 @@
+def mul_by_2(lis):
+    lis1=[]
+    lis1=[2*ele for ele in lis]
+    return(lis1)
+print(mul_by_2([2,3,4]))
---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Test case for multiplying a list elements with '2'
+import unittest
+
+from code import mul_by_2
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+
+        data = [10,20,30,40,50]
+        result = summation(data)
+        self.assertEqual(result, [20,40,60,80,100])
+
+if __name__ == '__main__':
+    unittest.main()
